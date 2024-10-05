67811d7a3a2154e79f63cae8f6b1032d83af656b
SuphasanManitsaKKU <suphasan.m@kkumail.com>
2024-10-05 15:40:56 +0700

    .

--- app/note/components/card.tsx
+++ app/note/components/card.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import Swal from 'sweetalert2';
 import axios from 'axios';
 import { set } from 'date-fns';
-import ToggleSwitch from './toggleswicth';
 
 interface CardProps {
     cardId: string;
